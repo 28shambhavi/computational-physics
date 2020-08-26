106b873a61068660dc5393864a22a2b60fbe8dc6
Shambhavi Singh <28shambhavi@gmail.com>
2020-08-26 19:56:49 +0530

    Lab2

--- Labs/Bisection2.m
+++ Labs/Bisection2.m
@@ -1,20 +1,18 @@
 func = @(x) tan(x) -x ;
 
 bisection(func,2,4.7,0.001,0.001)
-
+%bisection function has a void return type
 
 function bisection(f,x1,x2,d,c)
     l=0.0005; 
     a=0.01;
-    
     xmid=(x1+x2)/2;
     while (abs(f(xmid)) > d) && (x2-x1>c)
-    
-    if(f(xmid) * f(x2))<0
-        x1=xmid;
-    else
-        x2=xmid;
-    end
+        if(f(xmid) * f(x2))<0
+            x1=xmid;
+        else
+            x2=xmid;
+        end
     xmid=(x2+x1)/2;
     end
 
