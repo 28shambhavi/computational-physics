e146f6d597dba25fbda685f83adf9ca9cd7f51bb
shivangixgupta <shivangigupta1110@gmail.com>
2020-09-09 17:53:46 +0530

    lab4

---
+++ Labs/gaussian.m
@@ -0,0 +1,11 @@
+clc
+n=10:500; %plotting till upper limit since the calculations above this are taking very long
+time=[];
+for i=n
+    a=rand(i,i);
+    b=rand(i,1);
+    tic
+    x=a\b; % finding x using guassian function
+    time=[time,toc]; storing toc into time array
+end
+plot(n,time)
