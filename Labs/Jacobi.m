3081ba64ef65afc7ef23258bb5be9bcffcf823f0
Shambhavi Singh <28shambhavi@gmail.com>
2020-09-19 18:00:12 +0530

    Update Jacobi.m

--- Labs/Jacobi.m
+++ Labs/Jacobi.m
@@ -1,38 +1,25 @@
-close all;
-clc;
+function root = jacobi(a, b, x, n, error)
 
-A = { 8 2 3 1 ; ...
-      0 6 4 0 ; ...
-      2 3 9 3 ; ...
-      1 2 3 7 }
-
-B = {25 ; 24 ; 47 ; 42 }
-
-tol = 1e-6;
-
-[M,N] = size(A);
-if M~=N
-    error("A is not a square matrix")
-end
-
-for m = 1 : M
-    row = A(m,:);
-    disp(C)
-    d = sum(m)-row(m);
-    if row(m) <= d
-        error("A is not diagonally dominant")
+    D=zeros(n,n);
+    M=a;
+    for i=1:n
+        D(i, i)=a(i, i);
+        M(i, i)=0;
     end
+    
+    x0 = x;
+    
+    x= D\b - D\M*x0;
+    
+    while(norm(x-x0)>error)
+        x0 = x;
+    
+        x= D\b - D\M*x0;
+    end
+
+    root = x;
+
+    disp(x);  
+
 end
 
-x = ones(M,1);
-
-D = diag(diag(A));
-
-err = inf;
-while err > tol
-    dx = D\{b - A*x};
-    x =x + dx;
-    err = max(abs (dx./x));
-end
-
-disp(x)
